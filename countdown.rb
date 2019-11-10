920dfbc6de33ebd2297ca1ebd2871c41704b36d8
Adrian Samaniego <llathander@gmail.com>
2019-11-10 22:51:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
-def countdown(seconds_to_midnight)
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    seconds_to_midnight -= 1
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
   end
   "HAPPY NEW YEAR!"
 end
